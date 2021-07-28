dd27cce8c295b91b669247240acfe67a40d2618f
akinsola <thomas.lawanson@gmail.com>
2021-07-28 18:09:12 +0100

    created SEO Page

---
+++ src/components/seo.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {GeneralShowInfo} from '../constants/general-show-info.js'
+</script>
+
+<svelte:head>
+  <title>{GeneralShowInfo.fullShowName}</title>
+  <meta name="description" content={GeneralShowInfo.description}>
+  <meta property="og:title" content={GeneralShowInfo.fullShowName}>
+  <meta property="og:type" content={"website"}>
+  <meta property="og:url" content={GeneralShowInfo.url}>
+  <meta property="twitter:title" content={GeneralShowInfo.fullShowName}>
+  <meta property="twitter:description" content={GeneralShowInfo.description}>
+</svelte:head>
