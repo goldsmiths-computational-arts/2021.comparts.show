91a0145ad80139ceef3493ef85f85bf46b6628bb
Armando Gonzalez <agonzalezsosto@gmail.com>
2021-07-28 22:08:15 +0100

    add marissa's code

--- src/components/one-pager.svelte
+++ src/components/one-pager.svelte
@@ -4,6 +4,7 @@
   import ArtistsCSV from '../utils/data/artists.csv'
   import { stores } from '@sapper/app'
   import UAParser from 'ua-parser-js'
+  import anime from 'animejs/lib/anime.es.js'
 
   var parser = new UAParser()
   const { session } = stores()
@@ -16,23 +17,98 @@
 
   let imageIdOne
   let imageIdTwo
+  let imageIdThree
+
+  let startPosXA = random(500, 1000)
+  let startPosYA = random(500, 1000)
+
+  const startPosXB = random(500, 1000)
+  const startPosYB = random(500, 1000)
+
+  let windowWidth
+  let windowHeight
 
   onMount(async () => {
     imageIdOne = Math.floor(random(1, 163)).toString().padStart(5, '0')
     imageIdTwo = Math.floor(random(1, 163)).toString().padStart(5, '0')
+    imageIdThree = Math.floor(random(1, 163)).toString().padStart(5, '0')
+
+    startPosXA = random(0, windowWidth)
+    startPosYA = random(0, windowHeight)
+
+    windowWidth = window.innerWidth
+    windowHeight = window.innerHeight
+
+    anime({
+      targets: '#myClip .blob-path',
+      d: [
+        {
+          value:
+            'M95,-109.2C143.2,-72.7,216.4,-61.9,232.3,-32C248.2,-2.2,206.8,46.6,170.7,88.7C134.7,130.7,103.9,165.9,67.2,175C30.6,184.2,-12.1,167.3,-52.4,150C-92.8,132.7,-130.8,115.1,-141.6,86.6C-152.3,58,-135.7,18.5,-134,-29.3C-132.4,-77.1,-145.6,-133.1,-125.7,-174.6C-105.8,-216.1,-52.9,-243.1,-14.8,-225.5C23.4,-207.8,46.7,-145.7,95,-109.2Z',
+        },
+        {
+          value:
+            'M171.5,-191.9C215.3,-167.6,239.1,-107,234.1,-53.5C229,0.1,195,46.5,157.5,71.5C119.9,96.5,78.8,99.9,37.7,123.4C-3.3,146.9,-44.2,190.4,-72.4,185.9C-100.7,181.3,-116.1,128.7,-133.1,84.2C-150.1,39.7,-168.6,3.3,-177.6,-46.2C-186.7,-95.8,-186.2,-158.6,-155,-185.1C-123.8,-211.6,-61.9,-201.8,1,-202.9C63.9,-204.1,127.7,-216.2,171.5,-191.9Z',
+        },
+      ],
+      easing: 'easeInOutSine',
+      duration: 10000,
+      direction: 'infinite alternate',
+      loop: true,
+    })
+
+    anime({
+      targets: '#myClip2 .blob-path',
+      d: [
+        {
+          value:
+            'M171.5,-191.9C215.3,-167.6,239.1,-107,234.1,-53.5C229,0.1,195,46.5,157.5,71.5C119.9,96.5,78.8,99.9,37.7,123.4C-3.3,146.9,-44.2,190.4,-72.4,185.9C-100.7,181.3,-116.1,128.7,-133.1,84.2C-150.1,39.7,-168.6,3.3,-177.6,-46.2C-186.7,-95.8,-186.2,-158.6,-155,-185.1C-123.8,-211.6,-61.9,-201.8,1,-202.9C63.9,-204.1,127.7,-216.2,171.5,-191.9Z',
+        },
+        {
+          value:
+            'M95,-109.2C143.2,-72.7,216.4,-61.9,232.3,-32C248.2,-2.2,206.8,46.6,170.7,88.7C134.7,130.7,103.9,165.9,67.2,175C30.6,184.2,-12.1,167.3,-52.4,150C-92.8,132.7,-130.8,115.1,-141.6,86.6C-152.3,58,-135.7,18.5,-134,-29.3C-132.4,-77.1,-145.6,-133.1,-125.7,-174.6C-105.8,-216.1,-52.9,-243.1,-14.8,-225.5C23.4,-207.8,46.7,-145.7,95,-109.2Z',
+        },
+      ],
+      easing: 'easeInOutSine',
+      duration: 10000,
+      direction: 'infinite alternate',
+      loop: true,
+    })
+
+    anime({
+      targets: '#myClip3 .blob-path',
+      d: [
+        {
+          value:
+            'M171.5,-191.9C215.3,-167.6,239.1,-107,234.1,-53.5C229,0.1,195,46.5,157.5,71.5C119.9,96.5,78.8,99.9,37.7,123.4C-3.3,146.9,-44.2,190.4,-72.4,185.9C-100.7,181.3,-116.1,128.7,-133.1,84.2C-150.1,39.7,-168.6,3.3,-177.6,-46.2C-186.7,-95.8,-186.2,-158.6,-155,-185.1C-123.8,-211.6,-61.9,-201.8,1,-202.9C63.9,-204.1,127.7,-216.2,171.5,-191.9Z',
+        },
+        {
+          value:
+            'M95,-109.2C143.2,-72.7,216.4,-61.9,232.3,-32C248.2,-2.2,206.8,46.6,170.7,88.7C134.7,130.7,103.9,165.9,67.2,175C30.6,184.2,-12.1,167.3,-52.4,150C-92.8,132.7,-130.8,115.1,-141.6,86.6C-152.3,58,-135.7,18.5,-134,-29.3C-132.4,-77.1,-145.6,-133.1,-125.7,-174.6C-105.8,-216.1,-52.9,-243.1,-14.8,-225.5C23.4,-207.8,46.7,-145.7,95,-109.2Z',
+        },
+      ],
+      easing: 'easeInOutSine',
+      duration: 10000,
+      direction: 'infinite alternate',
+      loop: true,
+    })
 
     const moveCircles = () => {
       Array.from(document.querySelectorAll('.bubble-overlay')).forEach(
         (svgElement) => {
-          const circles = Array.from(
-            svgElement.querySelectorAll('.image-circle')
-          )
-          const cx = random(0, 100)
-          const cy = random(0, 100)
-          // console.log({ cx, cy })
+          const circles = Array.from(svgElement.querySelectorAll('.blob-path'))
+          const cx = random(0, windowWidth)
+          const cy = random(0, windowHeight)
+
           circles.forEach((circleEl) => {
-            circleEl.setAttribute('cx', cx)
-            circleEl.setAttribute('cy', cy)
+            // circleEl.setAttribute("transform", `translate(${cx},${cy})`);
+            anime({
+              targets: circleEl,
+              translateX: cx,
+              translateY: cy,
+              easing: 'easeInOutSine',
+              duration: 10000,
+            })
           })
         }
       )
@@ -75,60 +151,84 @@
         {`${artist.displayName}, `}
       {/each}
     </div>
+  </div>
 
-    <svg
-      preserveAspectRatio="xMinYMid meet"
-      class="bubble-overlay"
-      viewBox="0 0 100 100"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <clipPath id="myClip">
-        <circle class="image-circle" cx="40" cy="35" r="20" />
-      </clipPath>
+  <svg
+    class="bubble-overlay"
+    fill="none"
+    preserveAspectRatio="xMinYMid meet"
+    width={windowWidth}
+    height={windowHeight}
+    viewBox="0 0 {windowWidth} {windowHeight}"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <clipPath id="myClip">
+      <path
+        class="blob-path"
+        style="transform: translateX({startPosXA}px) translateY({startPosYA}px);"
+        d="M171.5,-191.9C215.3,-167.6,239.1,-107,234.1,-53.5C229,0.1,195,46.5,157.5,71.5C119.9,96.5,78.8,99.9,37.7,123.4C-3.3,146.9,-44.2,190.4,-72.4,185.9C-100.7,181.3,-116.1,128.7,-133.1,84.2C-150.1,39.7,-168.6,3.3,-177.6,-46.2C-186.7,-95.8,-186.2,-158.6,-155,-185.1C-123.8,-211.6,-61.9,-201.8,1,-202.9C63.9,-204.1,127.7,-216.2,171.5,-191.9Z"
+        fill="#000"
+      />
+    </clipPath>
+    <g clip-path="url(#myClip)">
       <image
         class="svg-overlay-img"
         href="./img/overlay-images/shivers_artist_{imageIdOne}.png"
         clip-path="url(#myClip)"
       />
-      <circle
-        class="image-circle"
-        vector-effect="non-scaling-stroke"
-        cx="40"
-        cy="35"
-        r="20"
-        stroke="black"
-        stroke-width="3"
-        fill="none"
-      />
-    </svg>
+    </g>
+  </svg>
 
-    <svg
-      preserveAspectRatio="xMinYMid meet"
-      class="bubble-overlay"
-      viewBox="0 0 100 100"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <clipPath id="myClip2">
-        <ellipse class="image-circle" cx="80" cy="5" rx="20" ry="15" />
-      </clipPath>
+  <svg
+    preserveAspectRatio="xMinYMid meet"
+    class="bubble-overlay"
+    width="1000"
+    height="1000"
+    fill="none"
+    viewBox="0 0 1000 1000"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <clipPath id="myClip2">
+      <path
+        class="blob-path"
+        d="M95,-109.2C143.2,-72.7,216.4,-61.9,232.3,-32C248.2,-2.2,206.8,46.6,170.7,88.7C134.7,130.7,103.9,165.9,67.2,175C30.6,184.2,-12.1,167.3,-52.4,150C-92.8,132.7,-130.8,115.1,-141.6,86.6C-152.3,58,-135.7,18.5,-134,-29.3C-132.4,-77.1,-145.6,-133.1,-125.7,-174.6C-105.8,-216.1,-52.9,-243.1,-14.8,-225.5C23.4,-207.8,46.7,-145.7,95,-109.2Z"
+        fill="#000"
+      />
+    </clipPath>
+    <g clip-path="url(#myClip2)">
       <image
         class="svg-overlay-img"
         href="./img/overlay-images/shivers_artist_{imageIdTwo}.png"
         clip-path="url(#myClip2)"
       />
-      <ellipse
-        class="image-circle"
-        vector-effect="non-scaling-stroke"
-        cx="80"
-        cy="5"
-        rx="20"
-        ry="15"
-        stroke="black"
-        stroke-width="3"
-        fill="none"
+    </g>
+  </svg>
+
+  <svg
+    preserveAspectRatio="xMinYMid meet"
+    class="bubble-overlay"
+    width="1000"
+    height="1000"
+    fill="none"
+    viewBox="0 0 1000 1000"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <clipPath id="myClip3">
+      <path
+        class="blob-path"
+        d="M95,-109.2C143.2,-72.7,216.4,-61.9,232.3,-32C248.2,-2.2,206.8,46.6,170.7,88.7C134.7,130.7,103.9,165.9,67.2,175C30.6,184.2,-12.1,167.3,-52.4,150C-92.8,132.7,-130.8,115.1,-141.6,86.6C-152.3,58,-135.7,18.5,-134,-29.3C-132.4,-77.1,-145.6,-133.1,-125.7,-174.6C-105.8,-216.1,-52.9,-243.1,-14.8,-225.5C23.4,-207.8,46.7,-145.7,95,-109.2Z"
+        fill="#000"
+        style="transform: translateX({startPosXB}px) translateY({startPosYB}px);"
       />
-    </svg>
-  </div>
+    </clipPath>
+    <g clip-path="url(#myClip3)">
+      <image
+        class="svg-overlay-img"
+        href="./img/overlay-images/shivers_artist_{imageIdThree}.png"
+        clip-path="url(#myClip3)"
+      />
+    </g>
+  </svg>
 </body>
 
 <style>
@@ -149,15 +249,8 @@
     text-align: justify;
   }
 
-  .names {
-    /*filter: blur(5px);*/
-  }
-
   .show-title {
     text-transform: uppercase;
-  }
-
-  .key-text {
   }
 
   .bubble-overlay {
@@ -171,11 +264,9 @@
   }
 
   .svg-overlay-img {
-    width: 200px;
-  }
-
-  .image-circle {
-    transition: all 10s ease;
+    width: 1500px;
+    y: 0;
+    x: 0;
   }
 
   .names:first-child {
