65d56dd02a2fb0a2112ead52e29f9b7e4b069a45
akinsola <thomas.lawanson@gmail.com>
2021-07-28 18:19:18 +0100

    added description

--- src/components/seo.svelte
+++ src/components/seo.svelte
@@ -6,6 +6,7 @@
   <title>{GeneralShowInfo.fullShowName}</title>
   <meta name="description" content={GeneralShowInfo.description}>
   <meta property="og:title" content={GeneralShowInfo.fullShowName}>
+  <meta property="og:description" content={GeneralShowInfo.description}>
   <meta property="og:type" content={"website"}>
   <meta property="og:url" content={GeneralShowInfo.url}>
   <meta property="twitter:title" content={GeneralShowInfo.fullShowName}>
