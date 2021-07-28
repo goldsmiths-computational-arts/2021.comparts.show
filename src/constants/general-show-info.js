db90a952283c174be11379b8e42c69a37d78e945
lxinspc <1971962+lxinspc@users.noreply.github.com>
2021-07-28 15:12:36 +0100

    removed subtitle from show info

--- src/constants/general-show-info.js
+++ src/constants/general-show-info.js
@@ -1,5 +1,5 @@
 const GeneralShowInfo = {
-    fullShowName: 'Shivers: Fractures in art and technology — Computational Arts MA/MFA Degree Show',
+    fullShowName: 'Shivers — Computational Arts MA/MFA Degree Show',
     description: ', 1st - 5th September 2021. Opening night Wednesday 1st September. Join us this September for the end of year exhibition of Goldsmiths Computational Arts Masters students. This exhibition will feature ground-breaking immersive performances, interactive installations, virtual and augmented realities, and thought-provoking conceptual works by mixed disciplinary artists from a diverse range of fields including fine art, design, psychology, music and creative computation. More information will be available via this website in the coming weeks.'
 }
 
